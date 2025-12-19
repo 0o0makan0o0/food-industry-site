35f1f7489333d3f2699a309cbb520791ed5ca0f2
0o0makan0o0 <134130337+0o0makan0o0@users.noreply.github.com>
2025-12-19 13:18:18 +0330

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function reveal() {
 window.addEventListener("scroll", reveal);
 
 // افکت تغییر ظاهر هدر هنگام اسکرول
-window.onscroll = function() {
+window.addEventListener("scroll", function() {
     const nav = document.querySelector('.navbar');
     if (window.scrollY > 50) {
         nav.style.padding = "5px 0";
@@ -23,13 +23,6 @@ window.onscroll = function() {
         nav.style.padding = "15px 0";
         nav.style.boxShadow = "none";
     }
-};
-
-// مدیریت ارسال فرم تماس (به صورت نمایشی)
-document.getElementById('contactForm')?.addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('ممنون از پیام شما! به زودی با شما در ارتباط خواهیم بود.');
-    this.reset();
 });
 
 // اجرای اولیه انیمیشن برای عناصر ابتدای صفحه
